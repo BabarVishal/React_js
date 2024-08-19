93c6d827e85a6d9a5e63aee2c846f3cb942e0bd9
vishal <vishalbabar7351@gmail.com>
2024-08-19 20:25:49 +0530

    Create Componats

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import React from 'react'
+import Chai from './chai'
+import MyButton from './MyButton'
+import Navbar from './Componats/navbar'
+
 function App() {
   return (
-    <>
-    <h1>Hy Vishal</h1>
-    </>
+    <div>
+      <Navbar/>
+      <h1>Vishal Babar</h1>
+      <Chai />
+      <MyButton />
+    </div>
   )
 }
 
 export default App
+
---
+++ src/MyButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MyButton() {
+  return (
+    <div>
+      <button>Click me</button>
+      <div className="myImg">
+       <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, quo.</h1>
+      </div>
+    </div>
+  )
+}
+
+export default MyButton
