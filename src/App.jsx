2fbf4cd8146de46dd997d52b9cf4dbbbaf0ecd6f
vishal <vishalbabar7351@gmail.com>
2024-08-20 15:45:39 +0530

    Componats

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Chai from './chai'
 import MyButton from './MyButton'
 import Navbar from './Componats/navbar'
+import Profile from './Componats/Profile'
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Profile />
       <h1>Vishal Babar</h1>
       <Chai />
       <MyButton />
---
+++ src/Componats/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div>
+        <img src="https://i.imgur.com/7vQD0fPs.jpg" />
+    </div>
+  )
+}
+
+export default Profile
